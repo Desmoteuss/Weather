ad7fe38f6b2ee245a5efdb67e6dc0077cdc19d84
desmoteuss <desmoteuss@gmail.com>
2020-03-24 18:55:40 +0100

    adding key

--- weather.js
+++ weather.js
@@ -1,1 +1,29 @@
 const key = '3rMuCg6fGlBIvJptMxiKHpiUgIAdu5zW';
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query =`${id}{}?apikey=${key}`;
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data[0];
+};
+
+const getCity = async (city)=>{
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query =`?apikey=${key}&q=${city}`;
+
+    const response = await fetch ( base + query);
+    const data = await response.json();
+    return data[0];
+
+};
+getCity('olsztyn').then(data => {
+    return getWeather ( data.Key);
+})
+.then(data => {
+    console.log(data)
+})
+.catch(err => console.log(err));
+
+
