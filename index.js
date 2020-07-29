3642b735b64cba4bf61a096a16ac17eaa4a3b781
desmoteuss <desmoteuss@gmail.com>
2020-07-29 20:41:54 +0200

    adding images

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
 
 const updateUI = (data)=> {
     const cityDets = data.citiDets;
     const weather = data.weather;
 
-    details.innerHTML=``;
+    details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>
+  `;
+
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute('src', iconSrc);
+  
+  let timeSrc = null;
+  if(weather.IsDayTime){
+    timeSrc = 'img/day.svg';
+  } else {
+    timeSrc = 'img/night.svg';
+  }
+  time.setAttribute('src', timeSrc);
+
+
   if(card.classList.contains('d-none')) {
 
   
     card.classList.remove('d-none');
     }
-}
+};
 
 const updateCity  = async (city) => {
 
